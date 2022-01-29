2d376b3e0f434ff4dad24b749fc300ed7d3b2f40
chris <christian.sean.friedrich@googlemail.com>
2022-01-29 19:45:26 +0100

    fixed minor errors from last push

--- muschel/logical/Commands.py
+++ muschel/logical/Commands.py
@@ -23,27 +23,32 @@ class Common(commands.Cog):
 
     @commands.command(name="random", brief="Zufallszahl zwischen [lB] und [uB] !random [lB] [uB]")
     async def getRandom(self, ctx, lB, uB):
-        await ctx.send(self.muschel.createRandom(int(lB), int(uB)))
+        try:
+            await ctx.send(self.muschel.createRandom(int(lB), int(uB)))
+        except ValueError:
+            await ctx.send("Parameter müssen int sein")
+        except Errors.InvalidArgumentsError:
+            await ctx.send("uB muss größer gleich (>=) lB sein")
+        except (Exception, BaseException) as e:
+            await ctx.send(f"oopsie woopsie sowwy TwT\n{e}")
 
     @commands.command()
     async def about(self, ctx):
         try:
             await ctx.send("help für mehr Informationen")
-        except ValueError:
-            await ctx.send("Parameter müssen int sein")
         except (Exception, BaseException) as e:
             await ctx.send(f"oopsie woopsie sowwy TwT\n{e}")
 
     @getFrage.error
     async def invalidArgs_getFragen_raise(self, ctx, error):
         await ctx.send("https://img-9gag-fun.9cache.com/photo/aDDjdzd_460s.jpg")
-        if isinstance(error, (commands.InvalidEndOfQuotedStringError or commands.MissingRequiredArgument)):
+        print(type(error))
+        if isinstance(error, (commands.InvalidEndOfQuotedStringError, commands.MissingRequiredArgument)):
             await ctx.send("ich kann die Nachricht nicht lesen")
+        elif isinstance(error, commands.UnexpectedQuoteError):
+            await ctx.send('Sag das nochmal')
 
     @getRandom.error
     async def invalidArgs_getRandom_raise(self, ctx, error):
-        print(type(error))
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Ungültige/Fehlende Argumente. 2 sind gegeben.")
-        elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("uB muss größer gleich (>=) lB sein")
