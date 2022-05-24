e8d9d58853d8901b1803bb4e8760572b65f9a580
prototype74 <46874884+prototype74@users.noreply.github.com>
2022-05-24 13:29:01 +0200

    main: use new logger

--- muschel/__main__.py
+++ muschel/__main__.py
@@ -1,3 +1,4 @@
+from muschel import log, __bot_logger__
 import muschel.logical.Commands as Commands
 from muschel.logical.Media_Reader import JSONReader
 from discord.ext import commands
@@ -9,7 +10,7 @@ bot.add_cog(Commands.Common())
 
 @bot.event
 async def on_ready():
-    print("bot ist bereit")
+    log.info("Bot ist online")
 
 
 def run_bot():
@@ -24,6 +25,7 @@ def run_bot():
 
 def main():
     run_bot()
+    log.info("Bot ist offline")
     return
 
 
@@ -31,4 +33,5 @@ if __name__ == '__main__':
     try:
         main()
     except (BaseException, Exception) as e:
-        print(f"{type(e).__name__}: {e}")
+        log.error(f"{type(e).__name__}: {e}", exc_info=True)
+    __bot_logger__._stop_logging()
