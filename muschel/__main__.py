3c4547a47cb6114e21dcd7037b834dfe321761b2
chris <christian.sean.friedrich@googlemail.com>
2022-09-02 12:00:35 +0200

    added asyncio (for async function calling, newly required) and discord root lib (for newly required intents (discord >=2.0)).
    made run_bot() and main() async.
    
    main(): changed bot...run_until_complete(start/close) to bot.start / bot.close, to be awaited, run_until_complete will be executed in __main__ call.
    
    __main__ call: added asyncio-loop to execute (run_until_complete) main() function.

--- muschel/__main__.py
+++ muschel/__main__.py
@@ -2,11 +2,13 @@ from muschel import cfgs, log, __bot_logger__
 import muschel.logical.Commands as Commands
 from muschel.logical.Media_Reader import JSONReader
 from discord.ext import commands
+import discord
+import asyncio
 import os
 
 reader = JSONReader(os.path.join("..", "media", "json", "credentials.json"))
-bot = commands.Bot(command_prefix=reader.getFileAttribute('prefix'))
-bot.add_cog(Commands.Common())
+intents = discord.Intents.default()  # intent argument required for discord-py >2.0
+bot = commands.Bot(command_prefix=reader.getFileAttribute('prefix'), intents=intents)
 
 
 @bot.event
@@ -14,28 +16,30 @@ async def on_ready():
     log.info("Bot ist online")
 
 
-def run_bot():
+async def run_bot():
     token = cfgs.getConfig("TOKEN")
+    await bot.add_cog(Commands.Common())
     if not token:
         raise ValueError("Discord bot token empty")
     try:
-        bot.loop.run_until_complete(bot.start(token))
+        await bot.start(token)
     except KeyboardInterrupt:
         pass
     finally:
-        bot.loop.run_until_complete(bot.close())
+        await bot.close(token)
     return
 
 
-def main():
-    run_bot()
+async def main():
+    await run_bot()
     log.info("Bot ist offline")
     return
 
 
 if __name__ == '__main__':
     try:
-        main()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
     except (BaseException, Exception) as e:
         log.error(f"{type(e).__name__}: {e}", exc_info=True)
     __bot_logger__._stop_logging()
