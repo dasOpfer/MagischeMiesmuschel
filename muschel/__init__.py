44af51926cfaa734e980749d7def8effb71526ca
prototype74 <46874884+prototype74@users.noreply.github.com>
2022-12-03 15:24:24 +0100

    init: print python and discord release

--- muschel/__init__.py
+++ muschel/__init__.py
@@ -1,6 +1,13 @@
 from muschel.utils.cfg_loader import _ConfigLoader
 from muschel.utils.logger import _Logger
+from discord import version_info as discord_ver
+from sys import version_info as py_ver
 from logging import getLogger
+
+print("===================")
+print(f"Python: v{py_ver.major}.{py_ver.minor}.{py_ver.micro}")
+print(f"Discord: v{discord_ver.major}.{discord_ver.minor}.{discord_ver.micro}")
+print("===================")
 
 # Start terminal logging
 log = getLogger(__name__)  # global usable logger
